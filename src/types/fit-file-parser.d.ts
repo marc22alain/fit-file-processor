45fd0ec6cabd95c14ace97f5de00b5b6619d7653
Marc de Niverville <marc@marcotypes.ca>
2025-02-05 22:50:15 -0500

    prompted: last compilation issues resolved
    thanks to Augment Code

--- src/types/fit-file-parser.d.ts
+++ src/types/fit-file-parser.d.ts
@@ -9,7 +9,21 @@ declare module 'fit-file-parser' {
     }
 
     export interface FitParserResult {
-        // Define the structure of the parsed result here
+        sessions?: Array<{
+            start_time?: Date;
+            end_time?: Date;
+            total_distance?: number;
+            avg_heart_rate?: number;
+            max_heart_rate?: number;
+            avg_power?: number;
+            max_power?: number;
+            [key: string]: any;
+        }>;
+        activities?: Array<{
+            event?: string;
+            event_type?: string;
+            [key: string]: any;
+        }>;
         [key: string]: any;
     }
 
